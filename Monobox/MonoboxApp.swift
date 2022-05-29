d486dc790227a56c9d16dd6d0b465d33aa22acc2
Darien Sandifer <dsis335@icloud.com>
2022-05-29 15:33:15 -0500

    updates to main content view

--- Monobox/MonoboxApp.swift
+++ Monobox/MonoboxApp.swift
@@ -11,7 +11,13 @@ import SwiftUI
 struct MonoboxApp: App {
     var body: some Scene {
         WindowGroup {
+            // use compiler directives #if to detect mac version or ios version
+            #if os(macOS)
+            // use min wi & he to make the start screen 800 & 1000 and make max wi & he to infinity to make screen expandable when user stretch the screen
+            ContentView().frame(minWidth: 800, minHeight: 800, alignment: .center)
+            #else
             ContentView()
+            #endif
         }
     }
 }
