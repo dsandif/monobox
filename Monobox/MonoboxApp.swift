f8b2b158b64f22f9e0cc00b80aecf3c5dd741125
Darien Sandifer <darien@goschoolbox.com>
2022-05-29 10:56:43 -0500

    Initial Commit

---
+++ Monobox/MonoboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonoboxApp.swift
+//  Monobox
+//
+//  Created by Darien Sandifer on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MonoboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
