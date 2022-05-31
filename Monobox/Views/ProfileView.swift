6328f33003eb8f34526dab802685afe12a11cee8
Darien Sandifer <dsis335@icloud.com>
2022-05-30 19:16:50 -0500

    added profile / preferences view

--- Monobox/Views/ProfileView.swift
+++ Monobox/Views/ProfileView.swift
@@ -8,14 +8,155 @@
 import SwiftUI
 
 struct ProfileView: View {
-    var body: some View {
-      HStack{
-        VStack(alignment: .center){
-          Rectangle()
-            .frame(width: 100, height: 100, alignment: .leading)
+  let firstColWidth = 180.0
+  let secondColWidth = 220.0
+  let prefColwidth = 70.0
+  var body: some View {
+    VStack(alignment: .leading){
+      profileHeader
+      Divider()
+        .padding(.bottom, 40)
+        .padding(.top, 20)
+      
+      VStack(alignment: .leading, spacing: 20){
+        userSettings
+        peopleSettings
+      }
+    }.padding()
+  }
+  var userSettings: some View{
+    Group{
+      preferences
+      Divider()
+      timeZone
+      Divider()
+      releases
+    }
+  }
+  var peopleSettings: some View {
+    Group{
+      Divider()
+      peopleInfo
+      Divider()
+      contacts
+      Divider()
+      mailbox
+    }
+  }
+  var preferences: some View {
+      Text("Preferences")
+        .font(Forza.headline)
+  }
+  
+  var timeZone: some View {
+    VStack{
+      HStack(alignment: .center){
+        Text("Automatic Time Zone")
+          .frame(width: firstColWidth, alignment: .leading)
+          .font(Forza.caption)
+        Spacer()
+        Text("GMT +2:00")
+          .frame(width: secondColWidth, alignment: .leading)
+          .font(Forza.caption)
+          .foregroundColor(.headings)
+        Spacer()
+        Text("Edit")
+          .font(Forza.caption)
+      }
+      .foregroundColor(.bodyText)
+    }
+    
+  }
+  var releases: some View {
+    VStack{
+      HStack(alignment: .center){
+        Text("Early Releases")
+          .frame(width: firstColWidth, alignment: .leading)
+          .font(Forza.caption)
+        Spacer()
+        Text("Get included in early releases")
+          .frame(width: secondColWidth, alignment: .leading)
+          .font(Forza.caption)
+          .foregroundColor(.headings)
+        Spacer()
+        Text("Edit")
+          .font(Forza.caption)
+      }
+      .foregroundColor(.bodyText)
+    }
+  }
+  var peopleInfo:some View {
+    VStack{
+      HStack(alignment: .center){
+        Text("People Info")
+          .frame(width: firstColWidth, alignment: .leading)
+          .font(Forza.caption)
+        Spacer()
+        Text("See info about people who view my files")
+          .frame(width: secondColWidth, alignment: .leading)
+          .font(Forza.caption)
+          .foregroundColor(.headings)
+        Spacer()
+        Text("Edit")
+          .font(Forza.caption)
+      }
+      .foregroundColor(.bodyText)
+    }
+  }
+  var contacts: some View {
+    VStack{
+      HStack(alignment: .center){
+        Text("Manually added contacts")
+          .frame(width: firstColWidth, alignment: .leading)
+          .font(Forza.caption)
+        Spacer()
+        Text("34 contacts")
+          .frame(width: secondColWidth, alignment: .leading)
+          .font(Forza.caption)
+          .foregroundColor(.headings)
+        Spacer()
+        Text("Edit")
+          .font(Forza.caption)
+      }
+      .foregroundColor(.bodyText)
+    }
+  }
+  var mailbox: some View {
+    VStack{
+      HStack(alignment: .center){
+        Text("Mailbox")
+          .frame(width: firstColWidth, alignment: .leading)
+          .font(Forza.caption)
+        Spacer()
+        Text("Add a mailbox for people to send files easily")
+          .frame(width: secondColWidth, alignment: .leading)
+          .font(Forza.caption)
+          .foregroundColor(.headings)
+        Spacer()
+        Text("Edit")
+          .font(Forza.caption)
+      }.foregroundColor(.bodyText)
+    }
+  }
+  
+  var profileHeader: some View {
+      HStack(alignment: .top, spacing: 30){
+        VStack(alignment: .center, spacing: 18){
+          Image("profileImg")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 90, height: 100, alignment: .center)
+            .cornerRadius(15)
+            .clipped()
+          
           Text("Edit Photo")
             .font(Forza.caption2)
+            .padding(.vertical,10)
+            .padding(.horizontal, 20)
+            .background(Color.lightBackground)
+            .cornerRadius(5)
         }
+        
         VStack(alignment: .leading, spacing: 20){
           HStack(alignment: .center){
             Text("Luka Doncic")
@@ -23,70 +164,54 @@ struct ProfileView: View {
               .kerning(-1)
             Spacer()
             Text("Edit")
+              .font(Forza.caption)
               .foregroundColor(.bodyText)
           }
-          
-          HStack(alignment: .top){
-            VStack(alignment: .leading, spacing: 20){
+          Divider()
+          HStack{
               Text("Email")
-              Text("Date Format")
-              Text("Security")
-              
-            }.foregroundColor(.bodyText)
-            
-            Spacer()
-            VStack(alignment: .leading, spacing: 20){
-              Text("lukaDoncic @mavericks.com")
-              Text("MM/DD/YYYY")
-              Text("Not Started")
+              .frame(width: prefColwidth, alignment: .leading)
+              Spacer()
+              Text("lukaDoncic @mavericksbball.com")
+                .frame(width: secondColWidth, alignment: .leading)
+                .foregroundColor(.headings)
+              Spacer()
+              Text("Edit")
             }
             .font(Forza.caption)
-            .foregroundColor(.headings)
-            
+            .foregroundColor(.bodyText)
+
+            Divider()
+          HStack{
+            Text("Date Format")
+              .frame(width: prefColwidth, alignment: .leading)
             Spacer()
-            VStack(alignment: .trailing, spacing: 20){
-              Text("Edit")
-              Text("Edit")
-              Text("Edit")
-            }.foregroundColor(.bodyText)
+              
+            Text("DD/MM/YYYY")
+              .frame(width: secondColWidth, alignment: .leading)
+              .foregroundColor(.headings)
+            Spacer()
+            Text("Edit")
           }
-          
-          
-//          HStack{
-//            Text("Email")
-//            Spacer()
-//            Text("lukaDoncic @mavericksbball.com")
-//              .foregroundColor(.headings)
-//            Spacer()
-//            Text("Edit")
-//          }
-//          .font(Forza.caption)
-//          .foregroundColor(.bodyText)
-//
-//          Divider()
-//          HStack{
-//            Text("Date Format")
-//            Spacer()
-//            Text("DD/MM/YYYY")
-//              .foregroundColor(.headings)
-//            Spacer()
-//            Text("Edit")
-//
-//          }
-//          .font(Forza.caption)
-//          .foregroundColor(.bodyText)
-//
-//          Divider()
-//          HStack{
-//            Text("Two-Factor Authentication")
-//            Spacer()
-//            Text("Start")
-//          }
-//          .font(Forza.caption)
-//          .foregroundColor(.bodyText)
+          .font(Forza.caption)
+          .foregroundColor(.bodyText)
+
+            Divider()
+          HStack{
+              Text("2FA")
+                .frame(width: prefColwidth, alignment: .leading)
+              Spacer()
+              Text("Not Started")
+                .frame(width: secondColWidth, alignment: .leading)
+                .foregroundColor(.headings)
+              Spacer()
+              Text("Start")
+            }
+            .font(Forza.caption)
+            .foregroundColor(.bodyText)
         }
       }
-    }
+  }
 }
 
 struct ProfileView_Previews: PreviewProvider {
